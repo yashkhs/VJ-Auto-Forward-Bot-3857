d14b9a2ca011f6b3ec2d7d7462f70ee84c592d89
yashkhs <yashneware2011@gmail.com>
2026-06-30 09:45:09 +0530

    Update configuration values in config.py
    Updated API_HASH, API_ID, BOT_TOKEN, and CHANNEL_ID in config.

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import getenv
 
 class Config(object):
-      API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
-      API_ID = int(getenv("API_ID", "20389440"))
+      API_HASH = getenv("API_HASH", "40e831848f06653377cccd11c1559f2f")
+      API_ID = int(getenv("API_ID", "21380877"))
       AS_COPY = True if getenv("AS_COPY", True) == "`{file_name}`" else True
-      BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001722984461:-1001623633000").replace("\n", " ").split(' '))
+      BOT_TOKEN = getenv("BOT_TOKEN", "7517034053:AAH6IW7Dwa5ObdIty04Ei1ohFUq5TXX_7QQ")
+      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001825299837:-1003572658105").replace("\n", " ").split(' '))
 
 
 # Don't Remove Credit @VJ_Botz
